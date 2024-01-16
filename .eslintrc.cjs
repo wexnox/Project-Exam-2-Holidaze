02d3f82071b304ab99006ac463a8431056219738
Lasse R <lasseronnlie@hotmail.com>
2024-01-16 09:36:10 +0100

    installed vite, react, react-router, axios, eslint, tailwind postcss, prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,41 +1,52 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: ['.eslintrc.{js,cjs}'],
-      parserOptions: {
-        sourceType: 'script',
-      },
+    root: true,
+    env: {
+        browser: true,
+        es2020: true,
     },
-  ],
-
-  ignorePatterns: [
-      'dist',
-    '.eslintrc.cjs'],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module' },
-  settings: {
-    react: {
-      version: '18.2'
-    } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react/jsx-runtime',
+        'plugin:react-hooks/recommended',
     ],
-  },
-}
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: ['.eslintrc.{js,cjs}'],
+            parserOptions: {
+                sourceType: 'script',
+            },
+        },
+    ],
+
+    ignorePatterns: [
+        'dist',
+        '.eslintrc.cjs',
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    settings: {
+        react: {
+            version: '18.2',
+        },
+    },
+    plugins: [
+        'react-refresh',
+        'prettier',
+    ],
+    rules: {
+        'prettier/prettier': 'error',
+        'react-refresh/only-export-components': [
+            'warn',
+            { allowConstantExport: true },
+        ],
+    },
+};
