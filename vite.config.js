b0e997b5e3126d37235bf16824adca65d50241be
Lasse R <lasseronnlie@hotmail.com>
2024-01-24 09:41:50 +0100

    added layout

--- vite.config.js
+++ vite.config.js
@@ -1,41 +1,39 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import autoprefixer from 'autoprefixer'
-import eslintPlugin from 'vite-plugin-eslint'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import autoprefixer from 'autoprefixer';
+import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-      react(),
-    eslintPlugin({
-      cache: false,
-      include: ['./src/**/*.js', './src/**/*.jsx'],
-      exclude: [],
-    }),
-  ],
-  // server: {
-  //      port: 3000,
-  //       proxy: {
-  //      '/api': {
-  //           target: 'http://localhost:5000',
-  //               changeOrigin: true,
-  //               secure: false,
-  //             },
-  //     },
-  // },
-  css: {
-    postcss: {
-      plugins: [
-        autoprefixer({})
-
-      ],
-    }
-  },
-  build: {
-    // rollupOptions: {
-    //   output: {
-    //     manualChunks: undefined,
-    //   },
+    plugins: [
+        react(),
+        eslintPlugin({
+            cache: false,
+            include: ['./src/**/*.js', './src/**/*.jsx'],
+            exclude: [],
+        }),
+    ],
+    // server: {
+    //      port: 3000,
+    //       proxy: {
+    //      '/api': {
+    //           target: 'http://localhost:5000',
+    //               changeOrigin: true,
+    //               secure: false,
+    //             },
+    //     },
     // },
-  },
-})
+    css: {
+        postcss: {
+            plugins: [autoprefixer({})],
+        },
+    },
+    build: {
+        sourcemap: true,
+        // rollupOptions: {
+        //   output: {
+        //     manualChunks: undefined,
+        //   },
+        // },
+    },
+});
