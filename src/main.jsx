332a9df59e08d3859d5049ec62b3673777f1ab21
Lasse R <lasseronnlie@hotmail.com>
2024-02-14 13:53:44 +0100

    updated main.jsx, add auth and Settings context API

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-import { AuthContextProvider } from './js/AuthContext.jsx';
+import { AuthContextProvider } from './components/context/AuthContext.jsx';
+import { SettingsContextProvider } from './components/context/SettingsContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <AuthContextProvider>
             <BrowserRouter>
-                <App />
+                <SettingsContextProvider>
+                    <App />
+                </SettingsContextProvider>
             </BrowserRouter>
         </AuthContextProvider>
-    </React.StrictMode>
+    </React.StrictMode>,
 );
