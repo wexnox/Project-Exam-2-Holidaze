3e6f0d441369080082e553b4bd29be8474c9f3c2
Lasse R <lasseronnlie@hotmail.com>
2024-02-14 13:39:51 +0100

    added routes/pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,40 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import Home from './pages/Home';
 import Login from './components/Login.jsx';
 import Registration from './components/Registration.jsx';
+import Venues from './pages/Venues.jsx';
+import VenueDetails from './pages/VenueDetails.jsx';
+import NotFound from './components/NotFound.jsx';
+import Profile from './pages/Profile.jsx';
+import About from './pages/About.jsx';
+import Contact from './pages/Contact.jsx';
+import Search from './pages/Search.jsx';
 
+// TODO: Convert to 6* routes. createBrowserRouter(createRoutesFromElements())
 function App() {
+
     return (
         <>
             <Routes>
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Home />} />
 
-                    {/*<Route path="profile" element={<Profile />} />*/}
+                    <Route path="profile" element={<Profile />} />
 
-                    {/*<Route path="accommodation" element={<Accommodation />} />*/}
-                    {/*<Route path="accommodation/:slug" element={<Accommodation />} />*/}
-
-                    {/*<Route path="about" element={<About />} />*/}
-                    {/*<Route path="contact" element={<Contact />} />*/}
+                    <Route path="about" element={<About />} />
+                    <Route path="contact" element={<Contact />} />
 
                     <Route path="login" element={<Login />} />
                     <Route path="register" element={<Registration />} />
 
-                    {/*<Route path="*" element={<NoMatch />} />*/}
+                    <Route path="venues" element={<Venues />} />
+                    <Route path="venues/venue-details/:id" element={<VenueDetails />} />
+                    <Route path="/search/:value" element={<Search />} />
+
+                    <Route path="*" element={<NotFound />} />
                 </Route>
             </Routes>
         </>
