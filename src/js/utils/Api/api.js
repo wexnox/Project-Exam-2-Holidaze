6c75300a0469a6b6b789756a8cd313afbee093dc
Lasse R <lasseronnlie@hotmail.com>
2024-02-14 13:39:04 +0100

    updated api+moved constants.js

--- src/js/utils/Api/api.js
+++ src/js/utils/Api/api.js
@@ -1,12 +1,79 @@
-// base
-const API_BASE_URL = 'https://api.noroff.dev/api/v1/holidaze/';
-// user
-const API_REGISTER = API_BASE_URL + 'auth/register';
-const API_LOGIN = API_BASE_URL + 'auth/login';
-const API_PROFILE = API_BASE_URL + 'profiles';
-// venues
-const API_VENUES = API_BASE_URL + 'venues';
-// Booking
-const API_BOOKING = API_BASE_URL + 'bookings';
+import axios from 'axios';
+import { useCallback, useState, useMemo, useContext } from 'react';
+import { AuthContext } from '../../../components/context/AuthContext.jsx';
+import { API_BASE_URL } from './constants.js';
 
-export { API_BASE_URL, API_REGISTER, API_LOGIN, API_PROFILE, API_VENUES, API_BOOKING };
+export function getApiClient(auth) {
+
+    return axios.create({
+        baseURL: API_BASE_URL,
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${auth}`,
+        },
+    });
+}
+
+async function ApiFetchCall(url, method, data, handlers, apiClient) {
+    const { setIsLoading, setIsError, setErrorMsg, setData, setCreated, setIsDeleted } = handlers;
+
+    try {
+        setIsLoading(true);
+        setIsError(false);
+        const response = await apiClient.request({
+            url: url,
+            method: method,
+            data: data,
+        });
+        if (response.status === 204) {
+            setIsDeleted((prevDeleted) => prevDeleted + 1);
+        } else {
+            setData(response.data);
+            setCreated(true);
+        }
+    } catch (error) {
+        setIsError(true);
+        setErrorMsg('An error occurred during the HTTP request');
+    } finally {
+        setIsLoading(false);
+    }
+}
+
+export const useApi = () => {
+    const [auth] = useContext(AuthContext);
+    const apiClient = useMemo(() => getApiClient(auth), [auth]);
+
+    const [apiState, setApiState] = useState({
+        data: [],
+        created: false,
+        isLoading: false,
+        isError: false,
+        errorMsg: '',
+        isDeleted: 0,
+    });
+
+    const handlers = useMemo(
+        () => ({
+            setIsLoading: (value) => setApiState((prevState) => ({ ...prevState, isLoading: value })),
+            setIsError: (value) => setApiState((prevState) => ({ ...prevState, isError: value })),
+            setErrorMsg: (value) => setApiState((prevState) => ({ ...prevState, errorMsg: value })),
+            setData: (value) => setApiState((prevState) => ({ ...prevState, data: value })),
+            setCreated: (value) => setApiState((prevState) => ({ ...prevState, created: value })),
+            setIsDeleted: (value) => setApiState((prevState) => ({ ...prevState, isDeleted: value })),
+        }),
+        [],
+    );
+
+    const fetchData = useCallback(
+        async (url, method = 'GET', postData) => {
+            try {
+                await ApiFetchCall(url, method, postData, handlers, apiClient);
+            } catch (e) {
+                console.error(e);
+            }
+        },
+        [handlers, apiClient],
+    );
+
+    return { ...apiState, fetchData };
+};
