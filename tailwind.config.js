6c5dcde6a0ccffd9130c6bf7ebd6e753367ec931
Lasse R <lasseronnlie@hotmail.com>
2024-02-14 13:42:37 +0100

    configured tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    plugins: [
+        require('@tailwindcss/typography'),
+        // ...
+    ],
 };
