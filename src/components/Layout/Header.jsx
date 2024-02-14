25bf4e51fa86ca01287b8727f452f31c90814970
Lasse R <lasseronnlie@hotmail.com>
2024-02-14 13:41:11 +0100

    removed uneeded fragment

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,10 +1,11 @@
+import React from 'react';
 import Navbar from './Navbar';
 
 const Header = () => {
     return (
-        <>
-            <Navbar />
-        </>
+
+        <Navbar />
+
     );
 };
 
