b4b25ecac86f738d54e2fcd9b918fb281ef861d8
Lasse R <lasseronnlie@hotmail.com>
2024-02-15 08:17:31 +0100

    added Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,88 @@
-import { useEffect } from 'react';
+import { useEffect, useContext } from 'react';
+import { API_VENUES } from '../js/utils/Api/constants.js';
+import VenueListItem from '../components/VenueListItem.jsx';
+import { getValidVenues } from '../js/utils/validation.js';
+import { SettingsContext } from '../components/context/SettingsContext.jsx';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const Headline = () => (
+    <section id={'headline'}>
+        <div className={'container mx-auto px-4 max-w-7xl'}>
+            <div className={'bg-cover bg-headline py-16 px-6 text-black'}>
+                <div id={'headline-content'} className={'sm:m-auto w-fit'}>
+                    <h1 className={'text-4xl font-bold mb-4'}>Welcome to Holidaze</h1>
+                    <p className={'mt-2 text-smg'}>Book your dream venue @ affordable prices</p>
+                    <p className={'mt-2 text-sm'}>Or turn your property into venue and rent it out</p>
+                </div>
+            </div>
+        </div>
+    </section>
+);
+
+const LatestVenues = ({ isError, data, isLoading }) => (
+    <section id={'latest-venues'} className={'mt-[88px] mb-12'}>
+        <div className={'container mx-auto px-4 max-w-7xl'}>
+            <h2 className={'text-3xl font-bold mb-12'}>Latest venues</h2>
+            {isLoading && (
+                <div className={'absolute left-0 right-0 min-h-screen flex justify-center bg-white'}>
+                    <div className={'loader'}></div>
+                </div>
+            )}
+            <div
+                id={'venues-container'}
+                className={`flex flex-col gap-14 sm:grid sm:grid-cols-2 sm:gap-x-8 sm:gap-y-12 md:grid-cols-3 lg:grid-cols-4`}
+            >
+                {!isLoading && isError && <ErrorMessage isError={isError} />}
+                {!isLoading && !isError && <VenueList data={data} />}
+            </div>
+        </div>
+    </section>
+);
+
+LatestVenues.propTypes = {
+    isError: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]),
+    data: PropTypes.array,
+    isLoading: PropTypes.bool,
+};
+
+const ErrorMessage = () => (
+    <div className={'api-error'}>
+        <p>Something went wrong..</p>
+        <p>Please try again later</p>
+    </div>
+);
+
+const VenueList = ({ data }) => {
+    return data && data.length > 0
+        ? getValidVenues(data)
+            .slice(0, 12)
+            .map((venue) => <VenueListItem key={venue.id} {...venue} />)
+        : null;
+};
+
+VenueList.propTypes = {
+    data: PropTypes.array,
+    isError: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]),
+};
 
 const Home = () => {
+    const { data, isLoading, isError, fetchData } = useContext(SettingsContext);
+
+    useEffect(() => {
+        fetchData(API_VENUES + '?&sort=created');
+    }, [fetchData]);
+
     useEffect(() => {
         document.title = 'Holidaze | Home';
     }, []);
+
     return (
         <>
-            <div className="container mx-auto relative">
-                <div className="bg-accent/20 mt-4 p-4 lg:shadow-xl lg:absolute  lg:left-0 lg:right-0 lg:p-0 lg:z-30 lg:-top-12"></div>
-                <div className="grid grid-cols-1 max-w-sm mx-auto gap-[30px] lg:grid-cols-3 lg:max-w-none lg:mx-0">Home @</div>
-            </div>
+            <main className={'mt-[120px] min-h-screen sm:mt-12'}>
+                <Headline />
+                <LatestVenues isLoading={isLoading} isError={isError} data={data} />
+            </main>
         </>
     );
 };
