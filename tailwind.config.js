5790a3ad08bede2cc91e54b97ff15db2074d0cf4
Lasse R <lasseronnlie@hotmail.com>
2024-02-17 06:59:29 +0100

    testing dark/light mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+    // purge: [],
+    // darkMode: 'class', // Enables dark mode based on the class applied to the HTML tag
+    // theme: {
+    //     extend: {},
+    // },
+    // variants: {},
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     plugins: [
         require('@tailwindcss/typography'),
