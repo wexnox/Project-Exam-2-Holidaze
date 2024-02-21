54ad3dd127db3a26735b3b1a6e79adae1bb7f497
Lasse R <lasseronnlie@hotmail.com>
2024-02-21 16:41:46 +0100

    expanding authcontext

--- src/components/context/AuthContext.jsx
+++ src/components/context/AuthContext.jsx
@@ -1,15 +1,38 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useContext, useState } from 'react';
 import PropTypes from 'prop-types';
 
-const AuthContext = createContext([null, () => {}]);
+const AuthContext = createContext({ accessToken: null });
 
 const AuthContextProvider = ({ children }) => {
     const [auth, setAuth] = useState();
+    const [venueManager, setVenueManager] = useState(false);
+    const [profileData, setProfileData] = useState({});
 
-    return <AuthContext.Provider value={[auth, setAuth]}>{children}</AuthContext.Provider>;
+    // Function to update profile data, can be called from any component within this context
+    const updateProfileData = (newData) => {
+        setProfileData((prevData) => ({ ...prevData, ...newData }));
+    };
+    // Function to toggle venue manager status
+    const toggleVenueManager = () => {
+        setVenueManager((prevStatus) => !prevStatus);
+    };
+
+    return <AuthContext.Provider value={[
+        auth,
+        setAuth,
+        venueManager,
+        setVenueManager,
+        profileData,
+        updateProfileData,
+    ]}>
+        {children}
+    </AuthContext.Provider>;
 };
 
 AuthContextProvider.propTypes = {
     children: PropTypes.node.isRequired,
 };
+// Custom hook to use auth context
+export const useAuth = () => useContext(AuthContext);
+
 export { AuthContext, AuthContextProvider };
