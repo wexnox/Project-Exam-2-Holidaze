20f9ca236eb203934a94faa10b5d808a77022df6
Lasse R <lasseronnlie@hotmail.com>
2024-02-21 16:42:01 +0100

    doc

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import Navbar from './Navbar';
 
+/**
+ * Represents the Header component.
+ *
+ * @returns {JSX.Element} The rendered Header component.
+ */
 const Header = () => {
     return (
 
