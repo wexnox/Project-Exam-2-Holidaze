703f1099d04927d12e28271442dce1de3bc9f5ed
Lasse R <lasseronnlie@hotmail.com>
2024-02-22 20:28:27 +0100

    Simplify AuthContext and remove unused functions.

--- src/components/context/AuthContext.jsx
+++ src/components/context/AuthContext.jsx
@@ -1,38 +1,11 @@
-import React, { createContext, useContext, useState } from 'react';
-import PropTypes from 'prop-types';
+import React, { createContext, useState } from 'react';
 
-const AuthContext = createContext({ accessToken: null });
+export const AuthContext = createContext([]);
 
-const AuthContextProvider = ({ children }) => {
-    const [auth, setAuth] = useState();
-    const [venueManager, setVenueManager] = useState(false);
-    const [profileData, setProfileData] = useState({});
+function AuthContextProvider({ children }) {
+    const [auth, setAuth] = useState(null);
 
-    // Function to update profile data, can be called from any component within this context
-    const updateProfileData = (newData) => {
-        setProfileData((prevData) => ({ ...prevData, ...newData }));
-    };
-    // Function to toggle venue manager status
-    const toggleVenueManager = () => {
-        setVenueManager((prevStatus) => !prevStatus);
-    };
+    return <AuthContext.Provider value={[auth, setAuth]}>{children}</AuthContext.Provider>;
+}
 
-    return <AuthContext.Provider value={[
-        auth,
-        setAuth,
-        venueManager,
-        setVenueManager,
-        profileData,
-        updateProfileData,
-    ]}>
-        {children}
-    </AuthContext.Provider>;
-};
-
-AuthContextProvider.propTypes = {
-    children: PropTypes.node.isRequired,
-};
-// Custom hook to use auth context
-export const useAuth = () => useContext(AuthContext);
-
-export { AuthContext, AuthContextProvider };
+export default AuthContextProvider;
