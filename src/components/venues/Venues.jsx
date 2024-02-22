c3eb281cf5f6dd91d06dfcf8e67b47f4e0aeb99c
Lasse R <lasseronnlie@hotmail.com>
2024-02-22 18:21:38 +0100

    Simplify Venues.jsx file by removing detailed comments

--- src/components/venues/Venues.jsx
+++ src/components/venues/Venues.jsx
@@ -1,22 +1,11 @@
-import { useContext, useEffect } from 'react';
+import React, { useContext, useEffect } from 'react';
 import VenueListItem from '../VenueListItem.jsx';
 import { API_VENUES } from '../../js/constants.js';
 import { getValidVenues } from '../../js/validation.js';
 import { SettingsContext } from '../context/SettingsContext.jsx';
-import PropTypes from 'prop-types';
-import React from 'react';
 import { FaArrowDownWideShort, FaArrowUpShortWide } from 'react-icons/fa6';
 
-/**
- * Render a button component for sorting purposes.
- *
- * @param {Object} props - The properties for the SortButton component.
- * @param {function} props.setActive - The function to toggle the active state of the button.
- * @param {boolean} props.active - The current active state of the button.
- * @param {React.ReactNode} props.children - The child elements to be rendered inside the button.
- * @param {Object} props... - Additional props to be spread onto the button element.
- * @return {JSX.Element} The rendered SortButton component.
- */
+
 function SortButton({ setActive, active, children, ...props }) {
     return (
         <button
@@ -29,86 +18,26 @@ function SortButton({ setActive, active, children, ...props }) {
     );
 }
 
-/**
- * PropTypes for SortButton component.
- *
- * @typedef {Object} SortButton.propTypes
- * @property {string} label - The label text for the button.
- * @property {boolean} ascending - The sorting direction flag (true for ascending, false for descending).
- * @property {function} onClick - The click event handler for the button.
- */
-SortButton.propTypes = {
-    setActive: PropTypes.func.isRequired,
-    active: PropTypes.bool.isRequired,
-    children: PropTypes.node.isRequired,
-};
-
-/**
- * Renders venue list based on the provided data and filter function.
- *
- * @param {Object[]} data - The array of venue objects.
- * @param {Function} filterFunction - The function used to filter the venue data.
- * @returns {JSX.Element[] | null} - An array of JSX elements representing the list of venues, or null if the input data is empty.
- */
 function VenuesPagination({ data, filterFunction }) {
     if (data.length > 0) {
         return filterFunction(data)
+            .slice(0, 20)
             .map((venue) => <VenueListItem key={venue.id} {...venue} />);
     }
     return null;
 }
 
-/**
- * Specifies the PropTypes for VenuesPagination component.
- *
- * @component
- * @category Component
- *
- * @param {number} totalVenues - The total number of venues.
- * @param {number} venuesPerPage - The number of venues to be displayed per page.
- * @param {number} currentPage - The current page number.
- * @param {func} onPageChange - The function to be called when the page changes.
- * @param {number} [pageRangeDisplayed=5] - The number of pages to be displayed in the pagination component.
- * @param {number} [marginPagesDisplayed=2] - The number of first and last pages to be displayed in addition to the page range.
- *
- * @returns {JSX.Element} The rendered VenuesPagination component.
- *
- * @throws {Error} Will throw an error if any of the required props are missing.
- */
-VenuesPagination.propTypes = {
-    data: PropTypes.array.isRequired,
-    filterFunction: PropTypes.func.isRequired,
-};
-
-/**
- * Render a UI component that represents an error response.
- *
- * @returns {JSX.Element} The rendered error response component.
- */
 function ErrorResponse() {
     return (
-        <div className={'api-error'}>
+        <div className={''}>
             <p>Something went wrong..</p>
             <p>Please try again later</p>
         </div>
     );
 }
 
-/**
- * Render venue section with sorting and pagination.
- * @returns {JSX.Element} Returns the rendered venue section.
- */
 function Venues() {
-    const {
-        data,
-        isLoading,
-        isError,
-        fetchData,
-        sort = false,
-        setSort,
-        sortOrder = false,
-        setSortOrder,
-    } = useContext(SettingsContext);
+    const [data, isLoading, isError, fetchData, , , , sort, setSort, sortOrder, setSortOrder] = useContext(SettingsContext);
 
     useEffect(() => {
         fetchData(`${API_VENUES}?&sort=${sort ? 'created' : 'name'}&sortOrder=${sortOrder ? 'desc' : 'asc'}`);
