12a693f6c8133adab45136a579052da494acf558
Lasse R <lasseronnlie@hotmail.com>
2024-02-22 20:34:06 +0100

    Update import statements in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-import { AuthContextProvider } from './components/context/AuthContext.jsx';
-import { SettingsContextProvider } from './components/context/SettingsContext.jsx';
+import AuthContextProvider from './components/context/AuthContext.jsx';
+import SettingsContextProvider from './components/context/SettingsContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
