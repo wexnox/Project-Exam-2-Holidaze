6887d9fe63aed434dd1afdc564a5972d03a4298f
Lasse R <lasseronnlie@hotmail.com>
2024-02-22 20:39:09 +0100

    Update Search component to use context and handle loading state.

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,8 +3,7 @@ import VenueListItem from './VenueListItem.jsx';
 import { API_VENUES } from '../js/constants.js';
 import React, { useContext, useEffect } from 'react';
 import { getValidVenues } from '../js/validation.js';
-import { SettingsContext } from './context/SettingsContext.jsx';
-import PropTypes from 'prop-types';
+import SettingsContext from './context/SettingsContext.jsx';
 
 function isSearchMatch(item, searchValue) {
     return item.name.toLowerCase().includes(searchValue) || item.description.toLowerCase().includes(searchValue);
@@ -16,7 +15,7 @@ function getSearchResults(data, searchValue) {
         : [];
 }
 
-function SearchResultsDisplay({ searchResults, value }) {
+function SearchResultsDisplay({ searchResults }) {
     return (
         <div id={'venues-container'}
              className="flex flex-col gap-14 sm:grid sm:grid-cols-2 sm:gap-x-8 sm:gap-y-12 md:grid-cols-3 lg:grid-cols-4">
@@ -27,15 +26,11 @@ function SearchResultsDisplay({ searchResults, value }) {
     );
 }
 
-SearchResultsDisplay.propTypes = {
-    searchResults: PropTypes.array.isRequired,
-    value: PropTypes.string.isRequired,
-};
-
 function Search() {
     const { value } = useParams();
-    const contextData = useContext(SettingsContext);
-    const { data, isError, fetchData } = contextData;
+    const [data, isLoading, isError, fetchData] = useContext(SettingsContext);
+    // const contextData = useContext(SettingsContext);
+    // const { data, isLoading, isError, fetchData } = contextData;
     const searchValue = value.toLowerCase();
     const searchResults = getSearchResults(data, searchValue);
 
@@ -48,6 +43,13 @@ function Search() {
             <section className="mt-10 mb-20 sm:mt-12">
                 <div className={'container mx-auto px-4 max-w-7xl'}>
                     <h1 className={'text-3xl font-bold mb-10'}>Search Results</h1>
+                    {isLoading && (
+                        <>
+                            <div className={'absolute left-0 right-0 min-h-screen flex justify-center bg-white'}>
+                                <div className={'loader'}></div>
+                            </div>
+                        </>
+                    )}
                     {!isError && (
                         <h2 className={'text-lg font-normal mb-4'}>
                             {searchResults.length > 0 ? 'Results for ' : 'No matches for '}{' '}
@@ -55,6 +57,12 @@ function Search() {
                         </h2>
                     )}
                     <SearchResultsDisplay searchResults={searchResults} value={value.trim()} />
+                    {isError && (
+                        <div className={'sm:w-fit'}>
+                            <p>Something went wrong..</p>
+                            <p>Please try again later</p>
+                        </div>
+                    )}
                 </div>
             </section>
         </main>
