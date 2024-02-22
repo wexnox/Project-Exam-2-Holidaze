dd66da9cd105bcc5bc4198a1617a7b60afe1c935
Lasse R <lasseronnlie@hotmail.com>
2024-02-22 18:19:14 +0100

    *

--- src/js/storage.mjs
+++ src/js/storage.mjs
@@ -1,6 +1,11 @@
+/**
+ * Retrieves the value of the specified key from the local storage.
+ *
+ * @param {string} key - The key of the item to retrieve from local storage.
+ * @return {any} The value of the item if it exists in local storage, otherwise an empty array.
+ */
 function getLocalStorage(key) {
     return localStorage.getItem(key) ? JSON.parse(localStorage.getItem(key)) : [];
 }
-
 
 export { getLocalStorage };
