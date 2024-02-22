770378fd99590d317487ba31ddabf96293e97de8
Lasse R <lasseronnlie@hotmail.com>
2024-02-22 18:15:16 +0100

    Simplify .eslintrc.cjs configuration file

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,69 +1,18 @@
 module.exports = {
-    root: true,
-    env: {
-        browser: true,
-        es2020: true,
-        node: true,
-    },
+    env: { browser: true, es2020: true },
     extends: [
         'eslint:recommended',
         'plugin:react/recommended',
         'plugin:react/jsx-runtime',
         'plugin:react-hooks/recommended',
-    ],
-    overrides: [
-        {
-            env: {
-                node: true,
-            },
-            files: ['**/*.jsx'],
-            parser: '@babel/eslint-parser',
-            parserOptions: {
-                requireConfigFile: false,
-                ecmaVersion: 2018,
-                sourceType: 'module',
-                ecmaFeatures: {
-                    jsx: true,
-                },
-                babelOptions: {
-                    presets: ['@babel/preset-react'],
-                },
-            },
-        },
-    ],
-
-    ignorePatterns: [
-        'dist',
-        '.eslintrc.cjs',
-        'src/main.jsx',
-    ],
-    parser: '@babel/eslint-parser',
-    parserOptions: {
-        requireConfigFile: false,
-        ecmaVersion: '2018',
-        sourceType: 'module',
-        ecmaFeatures: {
-            jsx: true,
-        },
-        babelOptions: {
-            presets: ['@babel/preset-react'],
-        },
-    },
-    settings: {
-        react: {
-            version: 'detect',
-        },
-    },
-    plugins: [
-        'react',
         'prettier',
-        'react-refresh',
     ],
+    parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+    settings: { react: { version: '18.2' } },
+    plugins: ['react-refresh'],
     rules: {
+        'react-refresh/only-export-components': 'warn',
+        'react/prop-types': 0,
         'no-unused-vars': 'off',
-        'react/prop-types': 'error',
-        'prettier/prettier': 'off',
-        'react/react-in-jsx-scope': 'off',
     },
-
 };
