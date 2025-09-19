adb6c664ef15aa50ad126fb492a823a48f27f2cd
Lasse R <lasseronnlie@hotmail.com>
2025-09-19 14:52:55 +0200

    Reformat Layout component

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -5,11 +5,13 @@ import { Outlet } from 'react-router-dom';
 
 function Layout() {
     return (
-        <>
+        <div className="min-h-screen flex flex-col">
             <Header />
-            <Outlet />
+            <main className="flex-1">
+                <Outlet />
+            </main>
             <Footer />
-        </>
+        </div>
     );
 };
 export default Layout;
