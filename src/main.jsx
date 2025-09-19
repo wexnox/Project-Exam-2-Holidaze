46ca558249ffb0cc6dda69eafa1ac1badb87658f
Lasse R <lasseronnlie@hotmail.com>
2025-09-19 07:34:45 +0200

    Reformat context providers

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-import AuthContextProvider from './components/context/AuthContext.jsx';
-import SettingsContextProvider from './components/context/SettingsContext.jsx';
+import AuthProvider from './components/context/AuthProvider.jsx';
+import { SettingsProvider } from './components/context/SettingsProvider.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <AuthContextProvider>
-            <BrowserRouter>
-                <SettingsContextProvider>
-                    <App />
-                </SettingsContextProvider>
-            </BrowserRouter>
-        </AuthContextProvider>
-    </React.StrictMode>,
+  <React.StrictMode>
+    <AuthProvider>
+      <BrowserRouter>
+        <SettingsProvider>
+          <App />
+        </SettingsProvider>
+      </BrowserRouter>
+    </AuthProvider>
+  </React.StrictMode>
 );
